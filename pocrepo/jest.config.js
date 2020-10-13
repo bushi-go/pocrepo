c1f3a86d22a1f1e32788f91272f01c08f11463f1
Romain Goussu <rgoussu.develop@gmail.com>
2020-10-13 09:46:00 +0000

    feat: added fooclient app, barclient app, common api, model library and component library

---
+++ pocrepo/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/fooclient',
+    '<rootDir>/apps/barclient',
+    '<rootDir>/apps/commonapi',
+    '<rootDir>/libs/components',
+    '<rootDir>/libs/model',
+  ],
+};
